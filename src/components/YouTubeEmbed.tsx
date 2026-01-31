a8cab5758fb167a628f2d5e64a80e13c145768a9
Urmzd Mukhammadnaim <urmzd.consulting@gmail.com>
2026-01-31 03:35:09 -0600

    feat: add seo and video embed components

---
+++ src/components/YouTubeEmbed.tsx
@@ -0,0 +1,19 @@
+export default function YouTubeEmbed({
+  id,
+  title,
+}: {
+  id: string;
+  title: string;
+}) {
+  return (
+    <div className="relative w-full aspect-video my-4 rounded-lg overflow-hidden">
+      <iframe
+        src={`https://www.youtube.com/embed/${id}`}
+        title={title}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="absolute inset-0 w-full h-full"
+      />
+    </div>
+  );
+}
