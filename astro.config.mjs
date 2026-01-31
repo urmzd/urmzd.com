c42ae72244c10dc9bd66a971b50cf782197da02f
Urmzd Mukhammadnaim <hello@urmzd.com>
2026-01-31 03:35:07 -0600

    chore: add justfile and update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://urmzd.com',
+  integrations: [react(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   vite: {
     plugins: [
       tailwindcss()
