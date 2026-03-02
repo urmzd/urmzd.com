ba33d0ba141223b09410fcf4b8d1f10b56005b64
Urmzd Mukhammadnaim <hello@urmzd.com>
2026-03-01 20:12:02 -0600

    feat(components): create component index for centralized exports
    Establish src/components/index.tsx as single entry point for component
    imports. Improves developer experience by reducing import path verbosity
    and centralizing component discovery. Enables easier component refactoring
    and organization.

---
+++ src/components/index.tsx
@@ -0,0 +1,44 @@
+export { default as ArchitectureFlow } from './ArchitectureFlow';
+export { default as Aside } from './Aside';
+export { default as AutoHideHeader } from './AutoHideHeader';
+export { default as BlockQuote } from './BlockQuote';
+export { default as BlogSearch } from './BlogSearch';
+export { default as ChatDemo } from './ChatDemo';
+export { default as CodeBlockEnhancer } from './CodeBlockEnhancer';
+export { default as Collapsible } from './Collapsible';
+export { default as CookieConsent } from './CookieConsent';
+export { default as CopyMarkdownButton } from './CopyMarkdownButton';
+export {
+  ConfirmationBiasVisual,
+  ConsilienceVisual,
+  CriticalThinkingLoop,
+  ExtrapolationVisual,
+  FirstPrinciplesVisual,
+  SearchLandscapeVisual,
+} from './CriticalThinkingVisuals';
+export { default as ExploreCard } from './ExploreCard';
+export { default as FeatureGrid } from './FeatureGrid';
+export { default as GPEvolutionVisualizer } from './GPEvolutionVisualizer';
+export { default as HeadingLinkEnhancer } from './HeadingLinkEnhancer';
+export { default as Hero } from './Hero';
+export { default as LandingExperience } from './LandingExperience';
+export { default as MobileTOC } from './MobileTOC';
+export { ModeToggle } from './ModeToggle';
+export { default as NavbarMenuEnhanced } from './NavbarMenuEnhanced';
+export { default as NavigationMenuDemo } from './NavigationMenuDemo';
+export { default as Phonetic } from './Phonetic';
+export { default as PreviewLink } from './PreviewLink';
+export { default as ProjectCard } from './ProjectCard';
+export { default as ProjectHero } from './ProjectHero';
+export { default as PullQuote } from './PullQuote';
+export { default as ScriptInline } from './ScriptInline';
+export { default as ScriptTranslator } from './ScriptTranslator';
+export { default as ShareButton } from './ShareButton';
+export { default as SocialDock } from './SocialDock';
+export { default as SocialLinksGrid } from './SocialLinksGrid';
+export { default as StatusBadge } from './StatusBadge';
+export { default as TableOfContents } from './TableOfContents';
+export { default as TechStackGrid } from './TechStackGrid';
+export { default as TimelineImage } from './TimelineImage';
+export { default as WelcomeTimeline } from './WelcomeTimeline';
+export { default as YouTubeEmbed } from './YouTubeEmbed';
